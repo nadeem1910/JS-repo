4708669552393ac269e5603650d103cc4e6349f2
Nadeem Khan <imkhan.1910@gmail.com>
2025-01-21 20:12:10 +0530

    code file added

---
+++ bom.js
@@ -0,0 +1,11 @@
+navigator.geolocation.getCurrentPosition((d)=>{ console.log(d); });
+console.log(location)
+console.log(location.hostname)
+console.log(location.host)
+console.log(location.pathname)
+console.log(location.href)
+console.log(location.port)
+console.log(history)
+
+
+
